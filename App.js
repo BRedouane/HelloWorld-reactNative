0315e0ec430095494b5c76f1a001bf68fdbd3c2d
BRedouane <111302616+BRedouane@users.noreply.github.com>
2023-05-03 10:39:47 -0400

    commentaire

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//c'est un peu comme les fonction react, on peu ajouter des variable et des sous fonction.
 export default function App() {
   return (
+    //ici, le view est plus l'equivalent de <div><div> dans HTML, tout les element doivent etre "entouré" d'un view,
+    // meme si il y a 2 view, ces dernier doivent etre dans un view qui les englobe,
+    //essayer de copier coller ces 3 ligne, et les coller juste en bas, ca fera un bug, ensuite entourer les par un view et ca fonctionnera
+    //la balise texte est comme le <p></p>, ca permet d'afficher le texte.
     <View style={styles.container}>
       <Text>Hello World!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
+//voir package.json...section script. c'est la ou on definie le comportement de l'application au demarage.
 
+//similaire au style dans Css, ici il crée des "bloc" de style avec un ID chacun, 
+//vous pouvez essayer de changer le backgroundColor à 'blue' pour voir
 const styles = StyleSheet.create({
   container: {
     flex: 1,
